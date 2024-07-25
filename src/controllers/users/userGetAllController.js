eb91c928f960cb7070bcc8eaf1082db7ec3d5a16
marcial <eliohernanco@gmail.com>
2024-07-25 01:33:54 -0400

    fix

--- src/controllers/users/userGetAllController.js
+++ src/controllers/users/userGetAllController.js
@@ -1,13 +1,21 @@
 import User from '../../models/User.js';
+import Friend from '../../models/Friends.js'; // Asegúrate de importar el modelo Friend
 
 export const getAllUsers = async (req, res) => {
   try {
-    // Assuming req.user.id contains the authenticated user's ID
+    // Suponiendo que req.user.id contiene el ID del usuario autenticado
     const authenticatedUserId = req.user.id;
 
+    // Obtiene los IDs de los amigos del usuario autenticado
+    const friendIds = await Friend.findOne({ idUser: authenticatedUserId }, 'friends')
+                                  .lean(); // Utiliza lean() para evitar la creación de documentos Mongoose
+    const friendIdsArray = friendIds ? friendIds.friends : [];
+
+    // Filtra los usuarios para excluir aquellos que son amigos del usuario autenticado
     const users = await User.find({ _id: { $ne: authenticatedUserId } })
                             .select('username profile.profile_picture')
-                            .sort('username');
+                            .sort('username')
+                            .where('_id').nin(friendIdsArray); // Usa $nin para excluir los IDs de los amigos
 
     res.json({
       message: {
